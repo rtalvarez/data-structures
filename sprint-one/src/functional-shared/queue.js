1f5736994e5c94231a7cdde350396eb7b6a8f411
Hack Reactor Students <robertoatc02@gmail.com>
2014-04-16 16:29:15 -0700

    finished functional and functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,37 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.len = 0;
+
+  _.extend(instance, queueMethods);
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  size: function(){
+    return this.len;
+  },
+
+  enqueue: function(value){
+    this.storage[this.len] = value;
+    this.len++;
+  },
+
+  dequeue: function(){
+    var result = this.storage[0];
+    delete this.storage[0];
+    for (var i = 1; i < this.len; i++){
+      this.storage[i - 1] = this.storage[i];
+    }
+    this.len && this.len--;
+    delete this.storage[this.len];
+    return result;
+  }
+
+
+
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,31 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  var instance = {};
+  instance.len = 0;
+  instance.storage = {};
+
+  _.extend(instance, stackMethods);
+
+  console.log(instance);
+  return instance;
 };
-  
-var stackMethods = {};
+
+
+var stackMethods = {
+
+  push: function(value){
+    this.storage[this.len] = value;
+    this.len++;
+  },
+
+  pop: function(){
+    this.len && this.len--;
+    var result = this.storage[this.len];
+    delete this.storage[this.len];
+    return result;
+  },
+
+  size: function(){
+    return this.len;
+  }
+
+};
